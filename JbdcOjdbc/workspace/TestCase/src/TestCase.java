94fd063b317d50f184680bfca2babe93b13c3713
SaiKrishnaIyer <ksai85075@gmail.com>
2024-03-17 14:37:42 +0530

    Initial commit

---
+++ JbdcOjdbc/workspace/TestCase/src/TestCase.java
@@ -0,0 +1,24 @@
+
+import java.sql.*;
+
+public class TestCase {
+	public static void main(String args[]){
+		try{
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			Connection con =DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","123456789");
+			
+			Statement stmt = con.createStatement();
+			
+			ResultSet rs=stmt.executeQuery("select * from students");
+			while(rs.next())
+			System.out.println(rs.getInt(1)+ " "+rs.getString(2)+" "+rs.getInt(3));
+			
+			con.close();
+			
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+}
